fe620bb5a6074e81a19e53aac2026c911a052093
WINDOWS 10 <WINDOWS 10@Windows10>
2018-09-21 11:10:34 +0700

    Latihan Mod3

---
+++ src/Latihan2/KonstruktorSuperKelas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Latihan2;
+
+/**
+ *
+ * @author WINDOWS 10
+ */
+public class KonstruktorSuperKelas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Employ programmer1 = new Employ("123456789", "Yusuf", 17);
+        programmer1.info();
+    }
+
+}
